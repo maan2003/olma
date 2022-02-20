82ddc6cb5673656ca32586e461f2688c92d5b3fe
Maan2003 <manmeetmann2003@gmail.com>
2022-02-20 08:57:32 +0530

    ViewExt

---
+++ src/view_ext.rs
@@ -0,0 +1,11 @@
+use druid_shell::piet::Color;
+
+use crate::{core::*, widgets::background::Background};
+
+pub trait ViewExt<'a>: View<'a> + Sized {
+    fn background(self, color: Color) -> Background<'a> {
+        Background::new(self).background(color)
+    }
+}
+
+impl<'a, V> ViewExt<'a> for V where V: View<'a> {}
